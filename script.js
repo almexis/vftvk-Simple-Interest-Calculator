2f8fc024f41087d8d93cd3556b5c78e534ce5780
almexis <48790050+almexis@users.noreply.github.com>
2022-02-10 14:01:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,8 +35,3 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-
-//check data entered into the principal
-//function checkdata() {
-
-//  }
